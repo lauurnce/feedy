a4b914902ab219637259e421f21138b38e48487d
lauurnce <paneslawrence8@gmail.com>
2025-08-08 15:00:34 +0800

    test(sources): cover _is_valid with a missing url

--- tests/test_base_validation.py
+++ tests/test_base_validation.py
@@ -7,3 +7,7 @@ def _entry(url="https://a.com", title="A"):
 
 def test_is_valid_accepts_entry_with_url_and_title():
     assert BaseFeedSource._is_valid(_entry()) is True
+
+
+def test_is_valid_rejects_entry_without_url():
+    assert BaseFeedSource._is_valid(_entry(url="")) is False
