1892385ad08f3cf989799df43c7c95773c879324
Lawrence Panes <paneslawrence8@gmail.com>
2026-05-26 02:16:33 +0800

    test(meta): add edge case tests for parse()

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -48,3 +48,49 @@ def test_parse_parses_date_to_iso(source):
     result = source.parse([_TWO_CARDS_HTML])
     assert result[0]["date"] == "2026-04-14"
     assert result[1]["date"] == "2026-03-25"
+
+
+_NO_TITLE_CARD_HTML = textwrap.dedent("""\
+    <html><body>
+    <a href="/blog/post/2026/01/01/no-title/">
+        <h6>JANUARY 1, 2026</h6>
+    </a>
+    </body></html>
+""")
+
+_ABSOLUTE_URL_HTML = textwrap.dedent("""\
+    <html><body>
+    <a href="https://developers.facebook.com/blog/post/2026/01/01/absolute/">
+        <h6>JANUARY 1, 2026</h6>
+        <h3>Absolute URL Post</h3>
+    </a>
+    </body></html>
+""")
+
+_NO_DATE_CARD_HTML = textwrap.dedent("""\
+    <html><body>
+    <a href="/blog/post/2026/01/01/no-date/">
+        <h3>No Date Post</h3>
+    </a>
+    </body></html>
+""")
+
+
+def test_parse_skips_card_with_no_title(source):
+    result = source.parse([_NO_TITLE_CARD_HTML])
+    assert result == []
+
+
+def test_parse_returns_empty_for_empty_input(source):
+    result = source.parse([])
+    assert result == []
+
+
+def test_parse_preserves_absolute_url(source):
+    result = source.parse([_ABSOLUTE_URL_HTML])
+    assert result[0]["url"] == "https://developers.facebook.com/blog/post/2026/01/01/absolute/"
+
+
+def test_parse_empty_date_on_missing_h6(source):
+    result = source.parse([_NO_DATE_CARD_HTML])
+    assert result[0]["date"] == ""
